baaa149e13e43b1fb2640f7fb59fc26e852764a5
Diana Carolina Hidalgo Guerrero <diana.hidalgo@globant.com>
2026-05-12 10:31:06 -0500

    Correción final de rutas para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/PM-WEBII-2026'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
